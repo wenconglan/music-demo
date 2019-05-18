d87bc04bc80d5ba3c41c4b95682c882a6c295e7c
logan <331089410@qq.com>
2019-05-18 16:45:56 +0800

    music s1.2

--- dist/scss.8c25d549.js
+++ dist/scss.8c25d549.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53827" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62284" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
